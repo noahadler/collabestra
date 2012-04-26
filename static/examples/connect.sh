a9e5afc21caeb3e851f66097aa0798fbcfc6d8db
Noah Adler <noah.adler@gmail.com>
2012-04-26 10:18:04 -0400

    Hide abc.js pad, make it ChucK only, and started adding inline docs/cheat sheet

--- static/examples/connect.sh
+++ static/examples/connect.sh
@@ -2,15 +2,15 @@
 while true
 do
 
-	wget http://localhost:9001/p/collexistra-score-pad/export/txt -O score.abc
-	abc2midi score.abc -o score.mid
+	#wget http://localhost:9001/p/collexistra-score-pad/export/txt -O score.abc
+	#abc2midi score.abc -o score.mid
 	wget http://localhost:9001/p/collexistra-chuck-pad/export/txt -O pad.ck
 
-	chuck --srate44100 pad.ck &
+	chuck --srate48000 pad.ck
 
-	aplaymidi -p 14:0 score.mid
+	#aplaymidi -p 14:0 score.mid
 
-	killall chuck
+	#killall chuck
 
 done
 
